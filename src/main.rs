cffa38229f4ff15a6cab33155e42e02d911d9d1f
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-21 19:30:19 +0530

    handling invalid input by changing lines 20 and 21

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,35 @@
 use rand::Rng;
-use std::io;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-
-    let mut secret_number = rand::thread_rng().gen_range(1.. 101);
+    let mut secret_number = rand::thread_rng().gen_range(1..101);
 
     println!("The Secret Number is: {}", secret_number);
 
-   loop{
-    println!("Please input your guess");
+    loop {
+        println!("Please input your guess");
 
-    let mut guess = String::new();
-    
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small"),
-        Ordering::Greater => println!("Too Big"),
-        Ordering::Equal => println!("You Win"),
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("You Win");
+                break;
+            }
+        }
     }
-   }
 }
