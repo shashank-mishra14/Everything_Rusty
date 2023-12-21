91444df0a461b1027d5da3b16536ee3cbd7d0e36
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-21 19:22:47 +0530

    making a comparision between the two objects using Ordering library

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use rand::Rng;
 use std::io;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Please input your guess");
@@ -9,10 +10,20 @@ fn main() {
     let mut secret_number = rand::thread_rng().gen_range(1.. 101);
 
     println!("The Secret Number is: {}", secret_number);
+
+    
     
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too Small"),
+        Ordering::Greater => println!("Too Big"),
+        Ordering::Equal => println!("You Win"),
+    }
 }
