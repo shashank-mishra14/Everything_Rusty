61c855f4b4a5809cbc6dec7f52bf83e2c83811da
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-21 19:17:12 +0530

    messed my brain right up

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
-    println!("Wssp here is your boy Rusteshwar");
-
     println!("Please input your guess");
 
     let mut guess = String::new();
 
-io::stdin().read_line(&mut guess)
-    .expect("Failed to read line");
+    let mut secret_number = rand::thread_rng().gen_range(1.. 101);
+
+    println!("The Secret Number is: {}", secret_number);
+    
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
 }
