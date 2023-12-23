299109c4421085b2218693499e7ab6e4d91b5245
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-23 14:15:07 +0530

    changes

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,18 @@ fn main(){
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+    //shadowing
+    let y=5;
+
+    println!("The value of y is: {}", y);
+
+    let y = 7;
+    println!("The changed value of y after shadowing  is: {}", y);
+
+    let y = "how are you";
+    println!("The changed value of y after shadowing  is: {}", y);
+
+
+    const SUB_COUNT: u32 = 100_000;
+    println!("The value of SUB_COUNT is: {}", SUB_COUNT);
 }
