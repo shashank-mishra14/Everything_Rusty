4d82c647a2985330455dffb04c454f0ad7ba150c
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-26 21:27:19 +0530

    ownership

---
+++ src/compound_types.rs
@@ -0,0 +1,7 @@
+fn main() {
+    //compound types
+    //tuple
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    println!("The value of tup is: {:?}", tup);
+}
+    
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,12 @@
-fn main(){
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
-    //shadowing
-    let y=5;
+    //ownership in Rust
+    
+    fn main(){
+        let s1 = String::from("hello");
+        let len = calculate_length(&s1);
+        println!("The length of '{}' is {}.", s1, len);
+    }
 
-    println!("The value of y is: {}", y);
-
-    let y = 7;
-    println!("The changed value of y after shadowing  is: {}", y);
-
-    let y = "how are you";
-    println!("The changed value of y after shadowing  is: {}", y);
-
-
-    const SUB_COUNT: u32 = 100_000;
-    println!("The value of SUB_COUNT is: {}", SUB_COUNT);
-}
+    fn calculate_length(s: &String) -> usize {
+        let length = s.len();
+        length
+    }
